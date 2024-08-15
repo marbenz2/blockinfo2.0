2fd39b04feed2835fff58d30626487bcc38f6b2d
marbenz2 <benzinger.marcel@gmail.com>
2024-08-15 15:23:30 +0200

    refactor: Update config with new token icons and logo

--- src/config/index.tsx
+++ src/config/index.tsx
@@ -3,3 +3,7 @@ export const NETWORK = (import.meta.env.VITE_NETWORK ?? "main") as
   | "test";
 export const NODE_URL =
   import.meta.env.VITE_NODE_URL ?? `https://${NETWORK}netc1.vechain.network`;
+
+export const VET_ICON = "VET_Token_Icon.webp";
+export const VTHO_ICON = "VTHO_Token_Icon.webp";
+export const VECHAIN_LOGO = "01-LogoVerticale_VShape_Colori_RGB.webp";
