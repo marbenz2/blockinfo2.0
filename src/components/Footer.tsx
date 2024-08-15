3541d9f30abf6698710a9dee3ea076b88f2a59df
marbenz2 <benzinger.marcel@gmail.com>
2024-08-15 15:42:29 +0200

    refactor: Update Footer component to fix copyright symbol and token icons

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,9 @@
 const Footer = () => {
   return (
     <footer className="flex w-full justify-center items-center gap-4 py-4 px-4 border-b bg-background text-sm">
-      <p>Copyright &copy; {new Date().getFullYear()} - MarBenz</p>
+      <p>&copy; {new Date().getFullYear()} - MarBenz</p>
       <p>||</p>
-      <p className="flex items-center gap-4">
+      <p className="flex items-center gap-2">
         powered by{" "}
         <img src="/VET_Token_Icon.webp" alt="vet-icon" className="w-4 h-4" />
         <img src="/VTHO_Token_Icon.webp" alt="vtho-icon" className="w-4 h-4" />
