245a9b227c03f80db0afafeacd909dd6cb36e5e7
marbenz2 <benzinger.marcel@gmail.com>
2024-08-15 15:20:50 +0200

    feat: Add Footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,15 @@
+const Footer = () => {
+  return (
+    <footer className="flex w-full justify-center items-center gap-12 py-4 px-4 border-b bg-background text-sm">
+      <p>Copyright &copy; {new Date().getFullYear()} - MarBenz</p>
+      <p>||</p>
+      <p className="flex items-center gap-4">
+        powered by{" "}
+        <img src="/VET_Token_Icon.webp" alt="vet-icon" className="w-4 h-4" />
+        <img src="/VTHO_Token_Icon.webp" alt="vtho-icon" className="w-4 h-4" />
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
