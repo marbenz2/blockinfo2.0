e7e8c4ad59dfb71fdf160d394c7f0d3879c114d6
marbenz2 <benzinger.marcel@gmail.com>
2024-08-15 17:41:25 +0200

    refactor: Improve UserAddress component loading state handling

--- src/components/UserInfo/UserAddress.tsx
+++ src/components/UserInfo/UserAddress.tsx
@@ -1,11 +1,13 @@
 import { useWalletName } from "@/hooks/use-wallet-name";
 import { useWallet } from "@vechain/dapp-kit-react";
-import { Skeleton } from "../ui/skeleton";
+import { useUserInfoStore } from "@/stores/user-info";
 import { CopyIcon } from "lucide-react";
 import { copyToClipboard, formatTxID } from "@/lib/utils";
 import { useToast } from "../ui/use-toast";
+import { Skeleton } from "../ui/skeleton";
 
 const UserAddress = () => {
+  const { isLoading } = useUserInfoStore();
   const wallet = useWallet();
   const { name } = useWalletName(wallet.account);
   const { toast } = useToast();
@@ -29,19 +31,27 @@ const UserAddress = () => {
       {!wallet.account && <p>Please Log In</p>}
       {wallet.account && (
         <>
-          {name ? (
-            <p>{name}</p>
-          ) : (
-            <Skeleton className="w-[150px] h-[20px] rounded-full bg-secondary" />
+          {isLoading && (
+            <>
+              <Skeleton className="w-[125px] h-[20px] rounded-full bg-secondary" />
+              <Skeleton className="w-[125px] h-[20px] rounded-full bg-secondary" />
+            </>
           )}
-          <p className="flex items-center gap-2 text-muted-foreground text-sm font-normal">
-            <CopyIcon
-              className="cursor-pointer"
-              size={12}
-              onClick={handleCopyClick}
-            />
-            ({wallet.account})
-          </p>
+          {!isLoading && name ? (
+            <>
+              <p>{name}</p>
+              <p className="flex items-center gap-2 text-muted-foreground text-sm font-normal">
+                <CopyIcon
+                  className="cursor-pointer"
+                  size={12}
+                  onClick={handleCopyClick}
+                />
+                ({wallet.account})
+              </p>
+            </>
+          ) : (
+            <p>{formatTxID(wallet.account)}</p>
+          )}
         </>
       )}
     </>
