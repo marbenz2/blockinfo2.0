6463b4d6c7b9543cdd33e8ca6ff9dcbdab15bba2
marbenz2 <benzinger.marcel@gmail.com>
2024-08-15 17:41:54 +0200

    refactor: Add XNodeImage component for displaying X node images and versions

--- src/config/index.tsx
+++ src/config/index.tsx
@@ -4,6 +4,10 @@ export const NETWORK = (import.meta.env.VITE_NETWORK ?? "main") as
 export const NODE_URL =
   import.meta.env.VITE_NODE_URL ?? `https://${NETWORK}netc1.vechain.network`;
 
+export const X_CONTRACT_ADDRESS =
+  import.meta.env.VITE_X_CONTRACT_ADDRESS ??
+  "0xb81E9C5f9644Dec9e5e3Cac86b4461A222072302";
+
 export const VET_ICON = "VET_Token_Icon.webp";
 export const VTHO_ICON = "VTHO_Token_Icon.webp";
 export const VECHAIN_LOGO = "01-LogoVerticale_VShape_Colori_RGB.webp";
